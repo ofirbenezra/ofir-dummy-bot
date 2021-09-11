091320e5b3c506a39833bf05d4687de04cf651ca
Ofir Ben Ezra <ofir.benezra@gmail.com>
2021-09-11 15:56:37 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const token = process.env.DISCORD_BOT_TOKEN;
+// require('dotenv').config();
+const { Client, Intents, ClientVoiceManager } = require("discord.js");
+// const { SlashCommandBuilder } = require('@discordjs/builders');
+// const { REST } = require('@discordjs/rest');
+// const { Routes } = require('discord-api-types/v9');
+const dotenv = require('dotenv');
+// const profanityChecker = require('./profanity-checker');
 
-client.on('ready', () => {
-    console.log("ready")
+dotenv.config();
+const token = process.env.DISCORD_BOT_TOKEN;
+console.log('Stating the Bot');
+const client = new Client({ 
+   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.login(process.env.TOKEN);
+
+client.on('ready', () => {
+   // List servers the bot is connected to
+   console.log("Servers:")
+   client.guilds.cache.forEach((guild) => {
+      console.log(" - " + guild.name + '' + guild.id)
+
+
+      // List all channels
+      guild.channels.cache.forEach((channel) => {
+         console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+      })
+   })
+   // const Guilds = client.guilds.cache.map(guild => guild.id);
+   //  console.log(Guilds);
+})
+
+
+console.log('before login');
+client.login(token);
