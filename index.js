6e11ceeadd7ccfb784dd9c986b9238f22fbb0160
Ofir Ben Ezra <ofir.benezra@gmail.com>
2021-09-11 12:44:34 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Test');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.DISCORD_BOT_TOKEN;
+
+client.on('ready', () => {
+    console.log("ready")
+});
+
+client.login(process.env.TOKEN);
